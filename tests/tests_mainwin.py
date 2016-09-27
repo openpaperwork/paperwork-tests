e30576768f96f1493f91040a3a2daa9b17595b38
Jerome Flesch <jflesch@gmail.com>
2016-09-27 15:23:22 +0200

    Add a test for vertical scrolling on the main canvas
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- tests/tests_mainwin.py
+++ tests/tests_mainwin.py
@@ -155,3 +155,44 @@ class TestBasicMainWin(unittest.TestCase):
             )
         finally:
             self.pw.stop()
+
+    def test_scroll_to_page(self):
+        self.pw.start()
+        try:
+            doc = self.pw.docsearch.get_doc_from_docid("20121213_1946_26")
+            self.pw.main_window.show_doc(doc)
+            self.pw.wait()
+
+            sc = pytestshot.screenshot(self.pw.gdk_window)
+            pytestshot.assertScreenshot(
+                self, "test_main_win_show_doc_multiple_pages", sc
+            )
+
+            self.pw.main_window.set_layout('paged')
+            self.pw.wait()
+
+            canvas = self.pw.main_window.img['canvas']
+
+            adj = canvas.get_hadjustment()
+            self.assertEqual(adj.get_value(), 0)
+            self.assertEqual(adj.get_lower(), 0)
+
+            adj = canvas.get_vadjustment()
+            self.assertEqual(adj.get_value(), 0)
+            self.assertEqual(adj.get_lower(), 0)
+            self.assertNotEqual(adj.get_upper(), 0)
+
+            target = int(adj.get_upper() / 2)
+            step = int(target / (1 / 0.05))
+            for pos in range(0, target, step):
+                adj.set_value(pos)
+                time.sleep(0.05)
+            adj.set_value(target)
+            self.pw.wait()
+
+            sc = pytestshot.screenshot(self.pw.gdk_window)
+            pytestshot.assertScreenshot(
+                self, "test_main_win_scroll_to_page", sc
+            )
+        finally:
+            self.pw.stop()
