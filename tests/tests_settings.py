88b47cc3061ade99d72248fcfe53319304d5d83b
Jerome Flesch <jflesch@gmail.com>
2016-09-27 20:51:19 +0200

    Settings: Check that 'OCR enabled' button works as intended
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- tests/tests_settings.py
+++ tests/tests_settings.py
@@ -14,6 +14,8 @@ gi.require_version('Gtk', '3.0')
 
 from gi.repository import GLib
 
+from paperwork.frontend.util.config import load_config
+
 
 class TestSettings(unittest.TestCase):
     def setUp(self):
@@ -33,3 +35,37 @@ class TestSettings(unittest.TestCase):
         finally:
             self.pw.stop()
         pytestshot.assertScreenshot(self, "test_settings_open", sc)
+
+    def test_uncheck_ocr(self):
+        self.pw.start()
+        try:
+            action = self.pw.main_window.actions['open_settings'][1]
+            GLib.idle_add(action.do)
+            try:
+                self.pw.wait()
+                self.assertNotEqual(action.dialog, None)
+                sc = pytestshot.screenshot(action.dialog.window.get_window())
+                pytestshot.assertScreenshot(self, "test_settings_open", sc)
+
+                widget = action.dialog.ocr_settings['enabled']['gui']
+                GLib.idle_add(widget.set_active, False)
+                self.pw.wait()
+
+                sc = pytestshot.screenshot(action.dialog.window.get_window())
+                pytestshot.assertScreenshot(
+                    self, "test_settings_uncheck_ocr", sc
+                )
+
+                GLib.idle_add(
+                    action.dialog.window.emit, 'delete-event', None
+                )
+                self.pw.wait()
+            finally:
+                GLib.idle_add(action.dialog.window.destroy)
+                self.pw.wait()
+        finally:
+            self.pw.stop()
+
+        config = load_config()
+        config.read()
+        self.assertFalse(config['ocr_enabled'].value)
