fa5519e7db61fd58338d06cc9c59862aa56bc261
Jerome Flesch <jflesch@openpaper.work>
2017-04-25 17:42:08 +0200

    add tests on file systems functions
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

---
+++ tests/tests_fs.py
@@ -0,0 +1,55 @@
+import os
+import unittest
+import tempfile
+
+import paperwork_backend.fs
+
+
+class TestGio(unittest.TestCase):
+    def setUp(self):
+        self.fs = paperwork_backend.fs.GioFileSystem()
+
+    def test_safe(self):
+        self.assertEqual(self.fs.safe("file:///home/jflesch/papers"),
+                         "file:///home/jflesch/papers")
+        self.assertEqual(self.fs.safe("/home/jflesch/papers"),
+                         "file:///home/jflesch/papers")
+
+    def test_basename(self):
+        self.assertEqual(self.fs.basename("file:///home/jflesch/toto.txt"),
+                         "toto.txt")
+        self.assertEqual(self.fs.basename("file:///home/jflesch/toto%21.txt"),
+                         "toto!.txt")
+
+    def test_dirname(self):
+        self.assertEqual(self.fs.dirname("file:///home/jflesch/toto.txt"),
+                         "file:///home/jflesch")
+
+    def test_join(self):
+        self.assertEqual(self.fs.join("file:///home/jflesch/kwain", "toto.txt"),
+                         "file:///home/jflesch/kwain/toto.txt")
+
+    def test_readwrite(self):
+        name = None
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            name = tmp_file.name
+        uri_name = self.fs.safe(name)
+        fd = None
+
+        with self.fs.open(uri_name, 'w') as _fd:
+            self.assertIsNotNone(_fd)
+            fd = _fd
+            fd.write(b"TEST_LINE\n")
+        self.assertTrue(fd.closed)
+
+        with self.fs.open(uri_name, 'r') as _fd:
+            self.assertIsNotNone(_fd)
+            fd = _fd
+            r = fd.read()
+            self.assertEqual(r, b"TEST_LINE\n")
+        self.assertTrue(fd.closed)
+
+        os.unlink(name)
+
+    def tearDown(self):
+        pass
